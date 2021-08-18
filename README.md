0b689dcc914ac890a6a0331b03c6a84a4bd1fadd
Steven Smith <github@icodeforyou.com>
2021-08-18 14:07:16 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# simple_bloc_observer
+
+Class for tracking changes (in development) of flutter_bloc
+
+- It uses flutter_bloc
+
+## Special Note
+
+Be kind to each other.
---
+++ lib/simple_bloc_observer.dart
@@ -0,0 +1,35 @@
+library simple_bloc_observer;
+
+// Copyright 2021 FHCP. All rights reserved.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// Custom [BlocObserver] which observes all bloc and cubit instances.
+/// NOTE: [Bloc.observer = SimpleBlocObserver();] before the call to [runApp] in [main.dart]
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    debugPrint('⚡ $event');
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onChange(BlocBase cubit, Change change) {
+    debugPrint('🦺$change');
+    super.onChange(cubit, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    debugPrint('⚙️ $transition');
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(BlocBase cubit, Object error, StackTrace stackTrace) {
+    debugPrint('🏮 $error');
+    super.onError(cubit, error, stackTrace);
+  }
+}
